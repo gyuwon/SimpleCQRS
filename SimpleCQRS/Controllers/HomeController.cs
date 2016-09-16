63a5c362207eeec428daeb4a7de702e2b6c16bf9
Gyuwon Yi <gyuwon@live.com>
2016-09-16 10:09:07 +0900

    Add project files.

---
+++ SimpleCQRS/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace SimpleCQRS.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [Route]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IHttpActionResult Index() =>
+            Redirect(new Uri("/swagger/ui/index", UriKind.Relative));
+    }
+}
---
+++ SimpleCQRS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleCQRS.WebApiApplication" Language="C#" %>
