4f82fd81edc02f2e80251506612874f93bc3de93
Gyuwon Yi <gyuwon@live.com>
2016-09-16 10:34:29 +0900

    데이터 모델을 구축합니다.

---
+++ SimpleCQRS/Models/Data/SimpleCQRSContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace SimpleCQRS.Models.Data
+{
+    public class SimpleCQRSContext : DbContext
+    {
+        public SimpleCQRSContext() : base("name=SimpleCQRSContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ SimpleCQRS/Models/Data/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SimpleCQRS.Models.Data
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 32, MinimumLength = 3)]
+        [Index(IsUnique = true)]
+        public string UserName { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+    }
+}
